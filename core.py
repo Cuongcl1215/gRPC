a6183dd68297b8c1e05f92b643f6b62a2514fbf7
Cuongcl1215 <nguyencuongcl1215@gmail.com>
2020-02-03 12:37:57 +0700

    update server.py

---
+++ core.py
@@ -0,0 +1,4 @@
+# Core function of the service
+def my_sum(x,y):
+    return x+y
+
--- server.py
+++ server.py
@@ -3,16 +3,14 @@ import message_pb2
 import message_pb2_grpc
 import time
 from concurrent import futures
-
-def my_sum(x,y):
-    return x+y
+import core
 
 # Create a wrapper class to generate response from input request
 class SumServicer(message_pb2_grpc.CalculateSumServicer):
     def my_sum(self, request, context):
         response = message_pb2.Sum()
         # get the value of the response by calling the defined function
-        response.sum = my_sum(request.addend1, request.addend2)
+        response.sum = core.my_sum(request.addend1, request.addend2)
         return response
 
 # Create a grpc server then add a servicer instance to that server
